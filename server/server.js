1a4a230fd268186745172c12a3e23dbfe146eb68
Russell Goldman <russellgoldman1@gmail.com>
2018-05-01 18:02:15 -0400

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -28,6 +28,15 @@ app.post('/todos', (req, res) => {
   });
 });
 
+// Attempt to get from the Todos route
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });

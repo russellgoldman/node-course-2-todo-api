e3414f7a731ffb9fadbab61e21b96c49ce5c16c2
Russell Goldman <russellgoldman1@gmail.com>
2018-05-13 06:16:18 -0400

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 
 // add the global promise key onto the mongoose object
 mongoose.Promise = global.Promise;
-// wire the database connection onto Mongoose (choose between MONGODB online database or MongoDB localhost database)
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+// wire the database connection onto Mongoose (online Mongo database)
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
   mongoose
